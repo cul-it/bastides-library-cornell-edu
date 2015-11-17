32aa11776ef1e0c48b6cbca1dd36d7e87d812cb2
starsplatter <jenncd@gmail.com>
2015-11-17 10:46:14 -0500

    mastheads through marciac

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -7,7 +7,14 @@ def link_help options={}
     options[:field] # the field to render
     options[:value] # the value of the field
     link_to content_tag(:span, 'Street View'), options[:value]
+end
 
+def has_location_selected?
+  location_param = params[:f][:location_facet_tesim]
+  if location_param.present?
+  @location_selected = location_param[0]
+  render "location_selected"
   end
+end
 
 end
